2206c06c88a3b2e12eacfa679a04f5c5a285cc97
Hasan Sağlam <hhasan.saglam@gmail.com>
2021-01-31 03:34:03 +0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpSaat = new System.Windows.Forms.GroupBox();
+            this.grpSaniye = new System.Windows.Forms.GroupBox();
+            this.grpDakika = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // grpSaat
+            // 
+            this.grpSaat.Location = new System.Drawing.Point(12, 12);
+            this.grpSaat.Name = "grpSaat";
+            this.grpSaat.Size = new System.Drawing.Size(642, 75);
+            this.grpSaat.TabIndex = 0;
+            this.grpSaat.TabStop = false;
+            this.grpSaat.Text = "Saat";
+            this.grpSaat.Enter += new System.EventHandler(this.grpSaat_Enter);
+            // 
+            // grpSaniye
+            // 
+            this.grpSaniye.Location = new System.Drawing.Point(12, 267);
+            this.grpSaniye.Name = "grpSaniye";
+            this.grpSaniye.Size = new System.Drawing.Size(642, 168);
+            this.grpSaniye.TabIndex = 0;
+            this.grpSaniye.TabStop = false;
+            this.grpSaniye.Text = "Saniye";
+            // 
+            // grpDakika
+            // 
+            this.grpDakika.Location = new System.Drawing.Point(12, 93);
+            this.grpDakika.Name = "grpDakika";
+            this.grpDakika.Size = new System.Drawing.Size(642, 168);
+            this.grpDakika.TabIndex = 0;
+            this.grpDakika.TabStop = false;
+            this.grpDakika.Text = "Dakika";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Location = new System.Drawing.Point(120, 457);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(0, 13);
+            this.lblSaat.TabIndex = 1;
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Location = new System.Drawing.Point(231, 457);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(0, 13);
+            this.lblDakika.TabIndex = 2;
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Location = new System.Drawing.Point(404, 457);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(0, 13);
+            this.lblSaniye.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 493);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.lblSaat);
+            this.Controls.Add(this.grpSaniye);
+            this.Controls.Add(this.grpDakika);
+            this.Controls.Add(this.grpSaat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpSaat;
+        private System.Windows.Forms.GroupBox grpSaniye;
+        private System.Windows.Forms.GroupBox grpDakika;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label lblSaniye;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayacBir = -1;
+        int sayacIki = -1;
+        int sayacUc = -1;
+        int saniye;
+        int dakika;
+        int saat;
+
+        Button btn = new Button();
+
+        private void grpSaat_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            lblSaat.Text = Convert.ToString(timer1);
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    sayacBir++;
+                    btn = new Button();
+                    btn.BackColor = Color.White;
+                    btn.Size = new Size(50, 25);
+                    btn.Left = ((20 + 30) * j) + 30;
+                    btn.Top = ((10 + 20) * i) + 10;
+                    btn.Text = sayacBir.ToString();
+                    btn.Name = btn.Text;
+                    grpSaat.Controls.Add(btn);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    sayacIki++;
+                    btn = new Button();
+                    btn.BackColor = Color.White;
+                    btn.Size = new Size(50, 25);
+                    btn.Left = ((20 + 30) * j) + 30;
+                    btn.Top = ((10 + 20) * i) + 10;
+                    btn.Text = sayacIki.ToString();
+                    btn.Name = btn.Text;
+                    grpDakika.Controls.Add(btn);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    sayacUc++;
+                    btn = new Button();
+                    btn.BackColor = Color.White;
+                    btn.Size = new Size(50, 25);
+                    btn.Left = ((20 + 30) * j) + 30;
+                    btn.Top = ((10 + 20) * i) + 10;
+                    btn.Text = sayacUc.ToString();
+                    btn.Name = btn.Text;
+                    grpSaniye.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            lblSaniye.Text = saniye.ToString();
+            lblDakika.Text = dakika.ToString();
+            lblSaat.Text = saat.ToString();
+
+            foreach (Button item in grpSaniye.Controls)     
+                item.BackColor = (item.Name == saniye.ToString() ? Color.Yellow : Color.White);              
+     
+
+
+            if (saniye == 60)
+            {
+                saniye = 0;
+                dakika++;
+                lblDakika.Text = dakika.ToString();
+
+                foreach (Button item in grpDakika.Controls)
+                    item.BackColor = (item.Name == dakika.ToString() ? Color.Yellow : Color.White);
+
+                if (dakika == 60)
+                {
+                    dakika = 0;
+                    saat++;
+                    lblSaat.Text = saat.ToString();
+
+                    if (saat == 24)
+                    {
+                        saniye = 0;
+                        dakika = 0;
+                        saat = 0;
+                    }
+                }
+            }
+        }
+    }
+}
